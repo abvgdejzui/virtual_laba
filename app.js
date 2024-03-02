5111a313edf7d99a6067fa355893a5ba8740d744
Plovmmm <91012438+Plovmmm@users.noreply.github.com>
2024-03-02 13:51:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ function save_IR() {
     var Ivitka = document.getElementById('I_vitk').value;
     var Rvitka = document.getElementById('R_vitk').value;
     var contentElement1 = document.getElementById('window1');
-        contentElement1.textContent = 'U = '+ 123 + ' В, R = ' + Rvitka +' Ом'
+        contentElement1.textContent = 'f_k = '+ 1 + ' кГц, I_k = ' + Ivitka +' мА'
     var contentElement2 = document.getElementById('window2');
-    contentElement2.textContent = 'I = '+ Ivitka + ' А, f = ' + 111 +' Вт'
+    contentElement2.textContent = 'E_з = '+ Rvitka + ' мВ, U_k = ' + 10 +' В'
 }
 window.global_I = Ivitka;
 window.global_R = Rvitka;
