478a59474af99ef6e97d879669b90074d44df0de
urusofam <68145778+urusofam@users.noreply.github.com>
2024-03-02 14:10:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var Ivitka = 10; var Rvitka = 10
-
+window.Ivitka = 10; 
+window.Rvitka = 10;
+window.res1 = (41783 * Ivitka / 1000) / (Math.sqrt(2) * Math.pow(10, 8));
 function save_IR() {
-    var Ivitka = document.getElementById('I_vitk').value;
+    Ivitka = document.getElementById('I_vitk').value;
+    res1 = (41783 * Ivitka / 1000) / (Math.sqrt(2) * Math.pow(10, 8));
     var Rvitka = document.getElementById('R_vitk').value;
     var contentElement1 = document.getElementById('window1');
         contentElement1.textContent = 'f_k = '+ 1 + ' кГц, I_k = ' + Ivitka +' мА'
     var contentElement2 = document.getElementById('window2');
     contentElement2.textContent = 'E_з = '+ Rvitka + ' мВ, U_k = ' + 10 +' В'
 }
-window.global_I = Ivitka;
-window.global_R = Rvitka;
 function exit() {
     window.location.href = "main.html"; // Замените на URL вашей страницы
 }
